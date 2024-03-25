e5ffae2594de4866850c35bfc7de95b097596386
tobbensol <tobsol01@gmail.com>
2024-03-25 14:44:24 +0100

    initial work to implement DB support

--- src/main.go
+++ src/main.go
@@ -5,6 +5,10 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Coordinate struct {
@@ -32,13 +36,28 @@ func add_coordinate(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "coordinate", Coordinate{X: x, Y: y})
 }
 
+func get_db_connection_str() string{
+	envFile, _ := godotenv.Read("../.env")
+	return envFile["connection_str"]
+}
+
+func setup_db() (*gorm.DB, error){
+	dsn := get_db_connection_str()
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return db, err
+}
+
 func main() {
+	
 	http.HandleFunc("/", mainSite)
 	http.HandleFunc("/add_coordinate/", add_coordinate)
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	fmt.Println("Server hosted at: http://localhost:8000/")
+
+	db, _ := setup_db()
+	fmt.Println(db.Name())
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
