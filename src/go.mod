4b8b1588fb146414e6b63e0c09d85d4e9e269fa7
tobbensol <tobsol01@gmail.com>
2024-09-13 21:34:53 +0200

    added mux as a http router for potential neat routes in the future

--- src/go.mod
+++ src/go.mod
@@ -10,6 +10,9 @@ require (
 )
 
 require (
+	github.com/gorilla/csrf v1.7.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
--- src/internal/Server/server.go
+++ src/internal/Server/server.go
@@ -2,6 +2,7 @@ package Server
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/tobbensol/elga_3_website/internal/Auth"
 	"github.com/tobbensol/elga_3_website/internal/Handlers"
 	"gorm.io/gorm"
@@ -10,16 +11,18 @@ import (
 )
 
 func Connect(db *gorm.DB) {
+	// mux will let me do neat routes in the future, right now it's equivalent to http.HandleFunc
+	r := mux.NewRouter()
 	// all the http request handlers
-	http.HandleFunc("/", Handlers.GetMainPage(db))
-	http.HandleFunc("/postReview/", Handlers.PostReview(db))
+	r.HandleFunc("/", Handlers.GetMainPage(db))
+	r.HandleFunc("/postReview/", Handlers.PostReview(db))
 
-	http.HandleFunc("/auth/login", Auth.Login)
-	http.HandleFunc("/auth/callback", Auth.Callback(db))
+	r.HandleFunc("/auth/login", Auth.Login)
+	r.HandleFunc("/auth/callback", Auth.Callback(db))
 
 	// host static files (css files, eventually images?)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("UI/static"))))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("UI/static"))))
 
 	fmt.Println("Server hosted at: http://localhost:8000/")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
