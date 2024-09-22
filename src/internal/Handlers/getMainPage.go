c9d404b00244ae5b3148e3277aa7d90fdec83c88
tobbensol <tobsol01@gmail.com>
2024-09-22 22:16:28 +0200

    changed postReview.go to use Templ

--- src/internal/Handlers/getMainPage.go
+++ src/internal/Handlers/getMainPage.go
@@ -2,7 +2,6 @@ package Handlers
 
 import (
 	"context"
-	"fmt"
 	"github.com/tobbensol/elga_3_website/src/UI/Templates"
 	"github.com/tobbensol/elga_3_website/src/internal/Models/Review"
 	"github.com/tobbensol/elga_3_website/src/internal/Models/User"
@@ -13,34 +12,35 @@ import (
 
 func GetMainPage(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var loggedIn bool
-		var user *User.User // Assuming your User model is named User and this is a pointer.
+		// default values
+		user := &User.User{}
+		loggedIn := false
 
 		// Attempt to retrieve the "code_verifier" cookie
 		userID, err := r.Cookie("authorization")
 		if err != nil {
 			// If there's an error (e.g., cookie not found), set loggedIn to false and log the error
-			loggedIn = false
 			log.Printf("Error retrieving cookie: %v", err)
 		} else {
 			// Cookie found, now attempt to find the user by their Discord ID
 			user, err = User.FindByDiscordID(db, userID.Value)
 			if err != nil {
 				// Handle the case where user is not found or there's an error
-				loggedIn = false
 				log.Printf("Error retrieving user by Discord ID: %v", err)
 			} else {
 				loggedIn = true
 			}
 		}
-		fmt.Println(user.GetAvatar())
 
 		// Render the main page template, passing the reviews and user status
 		reviews := Review.GetAll(db)
 		if loggedIn {
-			err = Templates.Main(reviews, loggedIn, *user).Render(context.Background(), w)
+			err = Templates.MainPage(reviews, loggedIn, *user).Render(context.Background(), w)
 		} else {
-			err = Templates.Main(reviews, loggedIn, User.User{}).Render(context.Background(), w)
+			err = Templates.MainPage(reviews, loggedIn, User.User{}).Render(context.Background(), w)
+		}
+		if err != nil {
+			log.Printf("Error rendering template: %v", err)
 		}
 	}
 }
--- src/internal/Handlers/postReview.go
+++ src/internal/Handlers/postReview.go
@@ -1,38 +1,33 @@
 package Handlers
 
 import (
-	"fmt"
+	"context"
+	"github.com/tobbensol/elga_3_website/src/UI/Templates"
 	"github.com/tobbensol/elga_3_website/src/internal/Models/Review"
 	"gorm.io/gorm"
-	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func PostReview(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("./src/UI/Templates/form.html"))
 		name := r.PostFormValue("Album_Name")
 		s := r.PostFormValue("Score")
 		scoreIsInt := true
+		review := Review.Review{}
 
 		score, err := strconv.ParseUint(s, 10, 8)
 		if err != nil {
 			scoreIsInt = false
+		} else {
+			review = Review.Create(db, name, uint8(score))
 		}
 
-		err = tmpl.ExecuteTemplate(w, "score error", scoreIsInt)
+		err = Templates.PostReview(review, scoreIsInt).Render(context.Background(), w)
 		if err != nil {
-			return
+			log.Printf("Error rendering template: %v", err)
 		}
 
-		if scoreIsInt {
-			review := Review.Create(db, name, uint8(score))
-			err = tmpl.ExecuteTemplate(w, "review", review)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
 	}
 }
